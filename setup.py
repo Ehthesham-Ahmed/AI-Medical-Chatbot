b03adfc6bca95e0d54770f2e5cc5b19f6c59b196
Ehthesham-Ahmed <117994804+Ehthesham-Ahmed@users.noreply.github.com>
2025-05-13 17:07:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ehthesham Ahmed',
+    author_email= 'syedehtheshamahmed@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
